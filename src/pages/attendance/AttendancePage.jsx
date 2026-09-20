9611de9589bf6dbd2c331bb670345b5aff35f031
cws31 <gautamrocky909621@gmail.com>
2026-09-20 10:47:29 +0530

    fix the import error

--- src/pages/attendance/AttendancePage.jsx
+++ src/pages/attendance/AttendancePage.jsx
@@ -12,7 +12,7 @@ import {
 
 import attendanceApi from "../../api/attendanceApi";
 import employeeApi from "../../api/employeeApi";
-import AttendanceSummary from "./AttendanceSummary";
+import AttendanceSummary from "../../components/attendance/AttendanceSummary";
 
 const STATUS = {
   PRESENT: "PRESENT",
